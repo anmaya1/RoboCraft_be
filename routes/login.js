4a6e67a63c8770d50a3b81657bf63d323eb497fe
Suhas <suhas4497@gmail.com>
2023-07-19 17:37:20 +0530

    password hashing

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,10 @@ const router=express.Router();
 const Login=require('../models/Login');
 const { body, validationResult } = require('express-validator');
 const bodyParser=require('body-parser');
+const bcrypt = require('bcryptjs')
 
-router.post('/',bodyParser.json(),[
+
+router.post('/createadmin',bodyParser.json(),[
     body('email','Enter a valid Email').isEmail(),
      body('password','Enter a valid Password').isLength({min: 8}),
 ],async (req,res)=>{
@@ -14,18 +16,24 @@ router.post('/',bodyParser.json(),[
       return res.status(400).json({ errors: errors.array() });
     }
     //check weather the admin with this email exists
-    let login =await Login.findOne({email: req.body.email});
-    if(login){
+    try{
+
+      let login =await Login.findOne({email: req.body.email});
+       if(login){
        return res.status(400).json({error: "sorry a admin with this email exits"})
-    }
-    admin= await Login.create({
+      }
+      const salt=await bcrypt.genSalt(10);
+      secPass= await bcrypt.hash(req.body.password,salt);
+
+      admin= await Login.create({
       email: req.body.email,
-      password: req.body.password,
+      password: secPass,
     })
-    // .then(login => res.json(login))
-    // .catch(err=>{console.log(err)
-    // res.json({error:'Please enter a unique email id',message: err.message})})
-    res.json({"Nice":"nice"})
+    res.json(admin)
+    }catch(error){
+      console.error(error.message);
+      res.status(500).send("Some error Occured");
+    }
 })
 
 module.exports = router;
